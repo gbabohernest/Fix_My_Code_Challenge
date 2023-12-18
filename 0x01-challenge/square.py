2e549388d579386441018c09647498c95ec197ec
Ernest <gbaboh.ernest@gmail.com>
2023-12-18 23:23:35 +0000

    add documentations

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,29 +3,42 @@
 
 
 class Square():
-    """A square classs"""
+    """A square classs representing a square.
 
-    # width = 0
-    # height = 0
+       Attr:
+        - width: Width of the square.
+        - height: Height of the square.
+    """
 
     def __init__(self, width=0, height=0):
-        """Initialize a square"""
+        """Initialize a square with a given width and height
+
+           Args:
+               @width: width of the square
+               @heigh: heigth of the square
+        """
         self.width = width
         self.height = height
 
-        # for key, value in kwargs.items():
-        #    setattr(self, key, value)
-
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Area of the square.
+
+            Returns: Area of the square
+        """
         return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """Return the perimeter of the square"""
+        """Caculates the permiter of the squre.
+
+           Returns: The perimeter of the square
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Pretty print a square"""
+        """Print a string representation of the square
+
+           Return: A string reprsentation of the square.
+        """
         return "{}/{}".format(self.width, self.height)
 
 
