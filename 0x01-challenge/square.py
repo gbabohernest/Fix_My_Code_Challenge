06a60081c9fd69820ea9c639e28cd2cf2e39b2b3
Ernest <gbaboh.ernest@gmail.com>
2023-12-18 23:10:40 +0000

    fix typo in method name, properly named the class to follow convention, add the definition of properties width and height in the init method

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Defines a square module"""
+"""Defines a square module."""
 
 
-class square():
+class Square():
     """A square classs"""
 
-    width = 0
-    height = 0
+    # width = 0
+    # height = 0
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, width=0, height=0, *args, **kwargs):
         """Initialize a square"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.width = width
+        self.height = height
+
+        # for key, value in kwargs.items():
+        #    setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """Return the permitter of the square"""
+    def perimeter_of_my_square(self):
+        """Return the perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -28,7 +31,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
